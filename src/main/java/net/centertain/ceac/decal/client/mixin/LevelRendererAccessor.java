60a7cc7fff396e9869f1d3eea7eeb2153cff08ac
CEntertain <TimGeru@gmx.de>
2026-09-11 00:06:52 +0200

    Removed unneeded LevelRenderer accessor

--- src/main/java/net/centertain/ceac/decal/client/mixin/LevelRendererAccessor.java
+++ src/main/java/net/centertain/ceac/decal/client/mixin/LevelRendererAccessor.java
@@ -1,26 +1,12 @@
 package net.centertain.ceac.decal.client.mixin;
 
 import com.mojang.blaze3d.pipeline.RenderTarget;
-import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.renderer.LevelRenderer;
-import net.minecraft.client.renderer.RenderType;
-import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
-import org.spongepowered.asm.mixin.gen.Invoker;
 
 @Mixin(LevelRenderer.class)
 public interface LevelRendererAccessor {
-    @Invoker("renderChunkLayer")
-    void ceac$renderChunkLayer(
-            RenderType renderType,
-            PoseStack poseStack,
-            double camX,
-            double camY,
-            double camZ,
-            Matrix4f projectionMatrix
-    );
-
     @Accessor("translucentTarget")
     RenderTarget ceac$getTranslucentTarget();
 }
