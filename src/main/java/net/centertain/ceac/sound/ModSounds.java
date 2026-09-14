30647344f07faec9a6ad1dd4204ab240795c42af
CEntertain <TimGeru@gmx.de>
2026-09-14 10:14:08 +0200

    Added UI sounds for precise placement

--- src/main/java/net/centertain/ceac/sound/ModSounds.java
+++ src/main/java/net/centertain/ceac/sound/ModSounds.java
@@ -15,6 +15,14 @@ public final class ModSounds {
 
     public static final RegistryObject<SoundEvent> DECAL_PLACEMENT = register("decal_placement");
     public static final RegistryObject<SoundEvent> DECAL_DELETION = register("decal_deletion");
+    public static final RegistryObject<SoundEvent> DECAL_CHANGE_GRID_SIZE = register("decal_change_grid_size");
+    public static final RegistryObject<SoundEvent> DECAL_DEPTH_DECREASE = register("decal_depth_decrease");
+    public static final RegistryObject<SoundEvent> DECAL_DEPTH_INCREASE = register("decal_depth_increase");
+    public static final RegistryObject<SoundEvent> DECAL_MOVE_NORMAL = register("decal_move_normal");
+    public static final RegistryObject<SoundEvent> DECAL_MOVE_PLANAR = register("decal_move_planar");
+    public static final RegistryObject<SoundEvent> DECAL_PLACEMENT_ERROR = register("decal_placement_error");
+    public static final RegistryObject<SoundEvent> DECAL_ROTATE = register("decal_rotate");
+    public static final RegistryObject<SoundEvent> DECAL_TILT = register("decal_tilt");
 
     private static RegistryObject<SoundEvent> register(String name) {
         ResourceLocation id = ResourceLocation.fromNamespaceAndPath(MOD_ID, name);
