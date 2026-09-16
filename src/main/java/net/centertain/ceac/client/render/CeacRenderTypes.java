0f3cd44ad9d6d9ad8ddb1a4ee483cd1a83486467
CEntertain <TimGeru@gmx.de>
2026-09-16 22:09:49 +0200

    New accessor & render type

--- src/main/java/net/centertain/ceac/client/render/CeacRenderTypes.java
+++ src/main/java/net/centertain/ceac/client/render/CeacRenderTypes.java
@@ -3,10 +3,14 @@ package net.centertain.ceac.client.render;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.DefaultVertexFormat;
 import com.mojang.blaze3d.vertex.VertexFormat;
+import net.minecraft.Util;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.client.renderer.RenderStateShard;
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.resources.ResourceLocation;
 import org.lwjgl.opengl.GL11;
+
+import java.util.function.Function;
 
 public final class CeacRenderTypes {
     public static final RenderType IN_WORLD_UI = RenderType.create(
@@ -38,6 +42,44 @@ public final class CeacRenderTypes {
                     ))
                     .createCompositeState(false)
     );
+
+    public final RenderType inWorldText(ResourceLocation texture) {
+        return RenderType.create(
+                "in_world_text",
+                DefaultVertexFormat.POSITION_COLOR_TEX_LIGHTMAP,
+                VertexFormat.Mode.QUADS,
+                256,
+                false,
+                false,
+                RenderType.CompositeState.builder()
+                        .setShaderState(new RenderStateShard.ShaderStateShard(
+                                GameRenderer::getRendertypeTextShader
+                        ))
+                        .setTextureState(new RenderStateShard.TextureStateShard(
+                                texture,
+                                false,
+                                false
+                        ))
+                        .setTransparencyState(new RenderStateShard.TransparencyStateShard(
+                                "translucent",
+                                () -> {
+                                    RenderSystem.enableBlend();
+                                    RenderSystem.defaultBlendFunc();
+                                },
+                                RenderSystem::disableBlend
+                        ))
+                        .setDepthTestState(new RenderStateShard.DepthTestStateShard(
+                                "always",
+                                GL11.GL_ALWAYS
+                        ))
+                        .setCullState(new RenderStateShard.CullStateShard(false))
+                        .setLightmapState(new RenderStateShard.LightmapStateShard(true))
+                        .setWriteMaskState(new RenderStateShard.WriteMaskStateShard(
+                                true, false
+                        ))
+                        .createCompositeState(false)
+        );
+    }
 
 //    public static final RenderType IN_WORLD_UI_TEXTURED = RenderType.create(
 //            "in_world_ui_textured",
