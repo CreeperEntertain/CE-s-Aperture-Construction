047e9b8446f223a36b4c7cf5727f4b7e7db2bf46
CEntertain <TimGeru@gmx.de>
2026-09-20 04:39:29 +0200

    Gave the slope a working collision shape

--- src/main/java/net/centertain/ceac/block/custom/material_shapes/MaterialShapeSlope.java
+++ src/main/java/net/centertain/ceac/block/custom/material_shapes/MaterialShapeSlope.java
@@ -1,9 +1,49 @@
 package net.centertain.ceac.block.custom.material_shapes;
 
 import net.centertain.ceac.block.custom.MaterialShape;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
+import org.jetbrains.annotations.NotNull;
 
 public class MaterialShapeSlope extends MaterialShape {
+    private static final VoxelShape SHAPE = makeShape();
+
     public MaterialShapeSlope(Properties properties) {
         super(properties);
+    }
+
+    @Override
+    public @NotNull VoxelShape getShape(
+            @NotNull BlockState state,
+            @NotNull BlockGetter level,
+            @NotNull BlockPos pos,
+            @NotNull CollisionContext context
+    ) {
+        return SHAPE;
+    }
+
+    private static VoxelShape makeShape() {
+        VoxelShape shape = Shapes.empty();
+
+        for (int x = 0; x < 16; x++) {
+            shape = Shapes.or(
+                    shape,
+                    Block.box(
+                            x,
+                            0,
+                            0,
+                            x + 1,
+                            x + 1,
+                            16
+                    )
+            );
+        }
+
+        return shape;
     }
 }
