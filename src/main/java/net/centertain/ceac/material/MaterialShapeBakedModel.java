821c4b68f2db33a27ed4f00ee9ba63b087159e08
CEntertain <TimGeru@gmx.de>
2026-09-21 22:24:21 +0200

    Revert "Fixed face orientations for the most part"
    This reverts commit 264601742d96200fde5e47ea4a64d9817ed07214.

--- src/main/java/net/centertain/ceac/material/MaterialShapeBakedModel.java
+++ src/main/java/net/centertain/ceac/material/MaterialShapeBakedModel.java
@@ -100,142 +100,32 @@ public class MaterialShapeBakedModel extends BakedModelWrapper<BakedModel> {
         VertexFormat format = DefaultVertexFormat.BLOCK;
 
         int stride = format.getIntegerSize();
-        int uvOffset = format.getOffset(2) / Integer.BYTES;
         int normalOffset = format.getOffset(4) / Integer.BYTES;
 
-        Vec3[] original = new Vec3[4];
         Vec3[] transformed = new Vec3[4];
-
-        double[] originalU = new double[4];
-        double[] originalV = new double[4];
-
-        TextureAtlasSprite sprite = quad.getSprite();
 
         for (int i = 0; i < 4; i++) {
             int offset = i * stride;
 
-            original[i] = new Vec3(
+            Vec3 point = new Vec3(
                     Float.intBitsToFloat(vertices[offset]),
                     Float.intBitsToFloat(vertices[offset + 1]),
                     Float.intBitsToFloat(vertices[offset + 2])
             );
 
-            transformed[i] = transformPoint(original[i], x, y, z);
-
-            float packedU = Float.intBitsToFloat(vertices[offset + uvOffset]);
-            float packedV = Float.intBitsToFloat(vertices[offset + uvOffset + 1]);
-
-            originalU[i] = (packedU - sprite.getU0()) / (sprite.getU1() - sprite.getU0());
-            originalV[i] = (packedV - sprite.getV0()) / (sprite.getV1() - sprite.getV0());
+            transformed[i] = transformPoint(point, x, y, z);
 
             vertices[offset] = Float.floatToRawIntBits((float) transformed[i].x);
             vertices[offset + 1] = Float.floatToRawIntBits((float) transformed[i].y);
             vertices[offset + 2] = Float.floatToRawIntBits((float) transformed[i].z);
         }
 
-        Vec3 originalNormal = original[1]
-                .subtract(original[0])
-                .cross(original[2].subtract(original[0]))
-                .normalize();
-
-        Vec3 transformedNormal = transformed[1]
+        Vec3 normal = transformed[1]
                 .subtract(transformed[0])
                 .cross(transformed[2].subtract(transformed[0]))
                 .normalize();
 
-        Vec3 originalEdge1 = original[1].subtract(original[0]);
-        Vec3 originalEdge2 = original[2].subtract(original[0]);
-
-        double du1 = originalU[1] - originalU[0];
-        double dv1 = originalV[1] - originalV[0];
-        double du2 = originalU[2] - originalU[0];
-        double dv2 = originalV[2] - originalV[0];
-
-        double determinant = du1 * dv2 - du2 * dv1;
-
-        if (Math.abs(determinant) > 1.0e-7) {
-            Vec3 originalVAxis = originalEdge2
-                    .scale(du1)
-                    .subtract(originalEdge1.scale(du2))
-                    .scale(1.0 / determinant)
-                    .normalize();
-
-            Vec3 originalUp = getLockedVAxis(originalNormal);
-
-            double textureAngle = Math.atan2(
-                    originalNormal.dot(originalUp.cross(originalVAxis)),
-                    originalUp.dot(originalVAxis)
-            );
-
-            Vec3 transformedUp = getLockedVAxis(transformedNormal);
-
-            Vec3 vAxis = rotateAroundAxis(
-                    transformedUp,
-                    transformedNormal,
-                    textureAngle
-            ).normalize();
-
-            Vec3 uAxis = transformedNormal.cross(vAxis).normalize();
-
-            if (uAxis.dot(
-                    transformed[1]
-                            .subtract(transformed[0])
-                            .scale(dv2)
-                            .subtract(transformed[2].subtract(transformed[0]).scale(dv1))
-                            .scale(1.0 / determinant)
-                            .normalize()
-            ) < 0.0) {
-                uAxis = uAxis.scale(-1.0);
-                vAxis = vAxis.scale(-1.0);
-            }
-
-            double minU = Double.POSITIVE_INFINITY;
-            double maxU = Double.NEGATIVE_INFINITY;
-            double minV = Double.POSITIVE_INFINITY;
-            double maxV = Double.NEGATIVE_INFINITY;
-
-            for (int i = 0; i < 4; i++) {
-                minU = Math.min(minU, originalU[i]);
-                maxU = Math.max(maxU, originalU[i]);
-                minV = Math.min(minV, originalV[i]);
-                maxV = Math.max(maxV, originalV[i]);
-            }
-
-            double minProjectedU = Double.POSITIVE_INFINITY;
-            double maxProjectedU = Double.NEGATIVE_INFINITY;
-            double minProjectedV = Double.POSITIVE_INFINITY;
-            double maxProjectedV = Double.NEGATIVE_INFINITY;
-
-            double[] projectedU = new double[4];
-            double[] projectedV = new double[4];
-
-            for (int i = 0; i < 4; i++) {
-                projectedU[i] = transformed[i].dot(uAxis);
-                projectedV[i] = transformed[i].dot(vAxis);
-
-                minProjectedU = Math.min(minProjectedU, projectedU[i]);
-                maxProjectedU = Math.max(maxProjectedU, projectedU[i]);
-                minProjectedV = Math.min(minProjectedV, projectedV[i]);
-                maxProjectedV = Math.max(maxProjectedV, projectedV[i]);
-            }
-
-            double projectedUSize = maxProjectedU - minProjectedU;
-            double projectedVSize = maxProjectedV - minProjectedV;
-
-            if (projectedUSize > 1.0e-7 && projectedVSize > 1.0e-7) {
-                for (int i = 0; i < 4; i++) {
-                    double u = minU + ((projectedU[i] - minProjectedU) / projectedUSize) * (maxU - minU);
-                    double v = minV + ((projectedV[i] - minProjectedV) / projectedVSize) * (maxV - minV);
-
-                    int offset = i * stride + uvOffset;
-
-                    vertices[offset] = Float.floatToRawIntBits(sprite.getU(u * 16.0));
-                    vertices[offset + 1] = Float.floatToRawIntBits(sprite.getV(v * 16.0));
-                }
-            }
-        }
-
-        int packedNormal = packNormal(transformedNormal);
+        int packedNormal = packNormal(normal);
 
         for (int i = 0; i < 4; i++)
             vertices[i * stride + normalOffset] = packedNormal;
@@ -244,41 +134,14 @@ public class MaterialShapeBakedModel extends BakedModelWrapper<BakedModel> {
                 vertices,
                 quad.getTintIndex(),
                 Direction.getNearest(
-                        transformedNormal.x,
-                        transformedNormal.y,
-                        transformedNormal.z
+                        normal.x,
+                        normal.y,
+                        normal.z
                 ),
                 quad.getSprite(),
                 quad.isShade(),
                 quad.hasAmbientOcclusion()
         );
-    }
-
-    private Vec3 getLockedVAxis(Vec3 normal) {
-        Vec3 worldUp = new Vec3(0, 1, 0);
-
-        Vec3 projected = worldUp.subtract(normal.scale(worldUp.dot(normal)));
-
-        if (projected.lengthSqr() <= 1.0e-7) {
-            Vec3 worldNorth = new Vec3(0, 0, -1);
-            projected = worldNorth.subtract(normal.scale(worldNorth.dot(normal)));
-        }
-
-        return projected.normalize();
-    }
-
-    private Vec3 rotateAroundAxis(
-            Vec3 vector,
-            Vec3 axis,
-            double angle
-    ) {
-        double cos = Math.cos(angle);
-        double sin = Math.sin(angle);
-
-        return vector
-                .scale(cos)
-                .add(axis.cross(vector).scale(sin))
-                .add(axis.scale(axis.dot(vector) * (1.0 - cos)));
     }
 
     private Vec3 transformPoint(
