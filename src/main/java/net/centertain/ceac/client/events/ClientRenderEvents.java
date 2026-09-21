ffb152e8c8c7f2dac2b77a57fe8fa93797a76040
CEntertain <74787699+CreeperEntertain@users.noreply.github.com>
2026-09-21 14:59:02 +0200

    Fixed typo

--- src/main/java/net/centertain/ceac/client/events/ClientRenderEvents.java
+++ src/main/java/net/centertain/ceac/client/events/ClientRenderEvents.java
@@ -98,7 +98,7 @@ public final class ClientRenderEvents {
                 PhysRenderHelper.getCleanPoseStack(),
                 bufferSource,
                 PhysRenderHelper.getProjectionMatrix()
-        );<
+        );
     }
     private static void materialHelpScreenRendering(MultiBufferSource.BufferSource bufferSource) {
         if (!(
