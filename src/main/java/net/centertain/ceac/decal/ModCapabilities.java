5657c2c771f39c5c4d239e095dfaf1857948e91e
CEntertain <74787699+CreeperEntertain@users.noreply.github.com>
2026-09-22 13:37:29 +0200

    Formatting changes for ModCapabilities

--- src/main/java/net/centertain/ceac/decal/ModCapabilities.java
+++ src/main/java/net/centertain/ceac/decal/ModCapabilities.java
@@ -6,8 +6,7 @@ import net.minecraftforge.common.capabilities.CapabilityManager;
 import net.minecraftforge.common.capabilities.CapabilityToken;
 
 public final class ModCapabilities {
-    public static final Capability<DecalManager> DECALS =
-            CapabilityManager.get(new CapabilityToken<>() {});
+    public static final Capability<DecalManager> DECALS = CapabilityManager.get(new CapabilityToken<>() {});
 
     private ModCapabilities() {}
 }
