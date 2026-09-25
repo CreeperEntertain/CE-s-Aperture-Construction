02cf773af8bcc8004e116be741d9d8758d851e1a
CEntertain <TimGeru@gmx.de>
2026-09-25 22:15:57 +0200

    Fixed AO rendering for all MaterialShapes, ever

---
+++ src/main/java/net/centertain/ceac/client/mixin/ForgeBlockModelRendererMixin.java
@@ -0,0 +1,69 @@
+package net.centertain.ceac.client.mixin;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import net.centertain.ceac.material.MaterialShapeBakedModel;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.resources.model.BakedModel;
+import net.minecraft.core.BlockPos;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.BlockAndTintGetter;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.client.model.data.ModelData;
+import net.minecraftforge.client.model.lighting.ForgeModelBlockRenderer;
+import net.minecraftforge.client.model.lighting.QuadLighter;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ForgeModelBlockRenderer.class)
+public abstract class ForgeBlockModelRendererMixin {
+    @Shadow @Final
+    private ThreadLocal<QuadLighter> smoothLighter;
+
+    @Inject(
+            method = "tesselateWithAO(Lnet/minecraft/world/level/BlockAndTintGetter;Lnet/minecraft/client/resources/model/BakedModel;Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/core/BlockPos;Lcom/mojang/blaze3d/vertex/PoseStack;Lcom/mojang/blaze3d/vertex/VertexConsumer;ZLnet/minecraft/util/RandomSource;JILnet/minecraftforge/client/model/data/ModelData;Lnet/minecraft/client/renderer/RenderType;)V",
+            at = @At("HEAD"),
+            cancellable = true,
+            remap = false
+    )
+    private void ceac$renderMaterialShapesWithSmoothAO(
+            BlockAndTintGetter level,
+            BakedModel model,
+            BlockState state,
+            BlockPos pos,
+            PoseStack poseStack,
+            VertexConsumer vertexConsumer,
+            boolean checkSides,
+            RandomSource rand,
+            long seed,
+            int packedOverlay,
+            ModelData modelData,
+            RenderType renderType,
+            CallbackInfo ci
+    ) {
+        if (!(model instanceof MaterialShapeBakedModel))
+            return;
+
+        ForgeModelBlockRenderer.render(
+                vertexConsumer,
+                smoothLighter.get(),
+                level,
+                model,
+                state,
+                pos,
+                poseStack,
+                checkSides,
+                rand,
+                seed,
+                packedOverlay,
+                modelData,
+                renderType
+        );
+
+        ci.cancel();
+    }
+}
